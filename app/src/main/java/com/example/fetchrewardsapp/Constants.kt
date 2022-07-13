d01a7c91f8ad4531f5a61c81a0a352381d263e19
lakeishamccree <lmccree215@icloud.com>
2022-07-13 05:56:59 -0400

    created the interface to make the network call

---
+++ app/src/main/java/com/example/fetchrewardsapp/Constants.kt
@@ -0,0 +1,7 @@
+package com.example.fetchrewardsapp
+
+object Constants {
+
+    const val fetchBaseUrl = "https://fetch-hiring.s3.amazonaws.com/"
+
+}
---
+++ app/src/main/java/com/example/fetchrewardsapp/network/FetchApi.kt
@@ -0,0 +1,26 @@
+package com.example.fetchrewardsapp.network
+
+import com.example.fetchrewardsapp.Constants
+import com.example.fetchrewardsapp.model.FetchListItem
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.create
+import retrofit2.http.GET
+
+interface FetchApi {
+
+    @GET("hiring.json")
+    suspend fun fetchItems(): List<FetchListItem>
+
+    companion object {
+        operator fun invoke(): FetchApi {
+            val retrofit = Retrofit.Builder()
+                .addConverterFactory(GsonConverterFactory.create())
+                .baseUrl(Constants.fetchBaseUrl)
+                .build()
+
+            return retrofit.create(FetchApi::class.java)
+        }
+
+    }
+}
